9d23158bec803eb7a3801f101ad53bdb30a159e5
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-07-18 00:33:17 +0200

    check allowed users

--- main.py
+++ main.py
@@ -12,10 +12,25 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+def is_user_allowed(user_id):
+    users_allowed = os.environ.get("TELEGRAM_ALLOWED_CHAT_IDS").split(",")
+
+    return str(user_id) in users_allowed
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+    if not is_user_allowed(update.effective_user.id):
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="You are not allowed to use this bot.")
+        return
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm ChatGPT, please talk to me!")
 
 async def chatgpt(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    if not is_user_allowed(update.effective_user.id):
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="You are not allowed to use this bot.")
+        return
+    
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Chatgpt: hi")
 
 if __name__ == '__main__':
@@ -23,7 +38,7 @@ if __name__ == '__main__':
     
     start_handler = CommandHandler('start', start)
     chatgpt_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), chatgpt)
-    
+
     application.add_handler(start_handler)
     application.add_handler(chatgpt_handler)
     
