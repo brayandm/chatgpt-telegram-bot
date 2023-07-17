87956ab0b83856f86a4f0c1c70e2c57c619b43db
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-07-18 00:18:42 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
+import logging
+import os
+
+load_dotenv()
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def chatgpt(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Chatgpt: hi")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(os.environ.get("TELEGRAM_BOT_TOKEN")).build()
+    
+    start_handler = CommandHandler('start', start)
+    chatgpt_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), chatgpt)
+    
+    application.add_handler(start_handler)
+    application.add_handler(chatgpt_handler)
+    
+    application.run_polling()
